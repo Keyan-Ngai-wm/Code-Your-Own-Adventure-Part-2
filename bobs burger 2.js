7783f406c81bd11970a6ce475e608ca2b4405380
keyan <session1@START-Coding-MBP-C17PN562G8WN.local>
2015-10-09 13:45:31 -0700

    Sorry this is late

---
+++ bobs burger 2.js
@@ -0,0 +1,83 @@
+confirm("Are you ready to go on the ultimate sanity test?");
+var age = prompt("What's your age");
+if (age < 11);
+{
+    confirm("You allowed to play but we don't take any responsibility.");
+}
+(age > 11);
+{
+    confirm("Play");
+}
+var user = 15;
+switch ( user ) {
+    case 15:
+        system.out.printIn("you're 15 ");
+        break;
+    case 16:
+        system.out.printIn("you're 16");
+        break;
+    case 17:
+        system.out.printIn("you're 17");
+        break;
+    case 18:
+        system.out.printIn("you're 18");
+        break;
+    default:
+        system.out.printIn("you're not olld enough to play but oh well.");
+
+}
+
+var userPartOne  = prompt("you're a lone hiker walking through the thick kelp forests of Bikini Bottom. And as you're cutting your way through  the thick masses of vegetation you finally reach a clearing. And in the center of the clearing you see a small group of children. A girl with a bright pink hat, a slightly overweight boy, and another child. But you could'nt really deciefer their gender. Due to your clumsiness you trip upon a a rock and fall face first into the ground. The commotion you've caused makes the children flinch and turn around. The one with the bunny ear hat spots you first and comes runnign over. Type C to continue  ");
+var userChoice = prompt("girl with the bunny ear hat- Well well well. What do we have here. Looks like we've got ourselves a little intruder. Tell me what is your name?");
+var userCont = prompt("Interesting name. Well I'm Louise, and I think I'll just call you Tuna cause you smell pretty funny. Anyways Tuna me and those dweebs back there need your help to get to the mysterious treasure hidden in that volcano over there. So what do ya say Tuna want guide us through unknown dangers and into the trecherous terain of that greatr volcano? I'll give you 3% of what we find.// Y for yes or N for no.");
+confirm("Louise-Ha ha ha  you're funny Tuna thinking you actually had a choice. You are so gullible. ");
+confirm("Louise- By the way dumb over there is my sister Tina, and Dumber who is standing next her is my brother Gene. ");
+confirm("Louise- Alright lets get walking. We don't want someone else getting there filthy dirty hands on MY TREASURE!!!");
+var yes = prompt("Tina- Hey Tuna do you think horses get songs stuck in their heads? Y or N");
+if (Yes > No);
+{
+    confirm("That's crazy isn't it! Like this chemistry I feel with you Tuna. ");
+}
+(Yes < No);
+{
+    confirm("you're wrong Tuna. Just imagine if you were suddenly a horse you'd probably already have a song stuck in your head. SO HORSES DO GET SONGS STUCK IN THERE HEAD!!!");
+}
+var userQ = prompt("Since horses get songs get stuck in their heads what songs do yu think get stuck?");
+confirm("Tina- I think my heart just pooped its pants. I can't believe you were thinking the same song aas me. Our love must be written in the bathroom stalls of every 7/11 out there!");
+confirm("Gene- Ah love. Reminds of those feelings I had for school lunch... UNTIL THAT MICHELLE OBAMA TOOK AWAY MY LOVE FROM ME... my precious lunch...");
+var userReaction = prompt("Gene breaks down crying . What do you do??");
+confirm("Gene- Just leave me to mourn my precious school lunch.");
+confirm("Louise- Hey Losers we've reached the entrance to the holy volcano of Fire!!.. Hmm it says for us to enter we must solve the puzzle.");
+var Johnny = prompt("Johnny's mom had three children. The first one was named April and the second child  was named May. What was the third childs name? write the name or type K to go ahead and ninja kick the door down like a boss  ");
+if (Johnny > K);
+{
+    confirm("Correct but you could've just kicked the door down it's made of cardboard. ");
+}
+(Johnnny < K);
+{
+    confirm("Hey there BOSS.");
+}
+var B = prompt ("As soon as you guys step into the entrance of the volcano an ugly goblin that looks like shrek pfft... or your mom. Either one. Anyways the goblin approaches you with a an angry look on its face. And out of nowhere it swings at you. What do you do? type C to curl up ina baall and cry like a loser, or type B to be brave and save your fellow companions from the goblin.");
+if (B > C);
+{
+    confirm("the goblin cowers in fear from your almightyness... You're such a bully.");
+}
+(B < C);
+{
+    confirm("Goblin reaches down to comfort you.");
+}
+confirm("Goblin-Hello itty bittys I'm Steve. welcome to the Holy Volcano that I am here to protect. Follow me this way so I can guide you to the treasure you seek.");
+var follow = prompt("Louise-Hmm he seems like a trusty goblin compared to the other of goblins I've seen. Should we follow? F to follow or D to disappear");
+if (follow > D);
+{
+    confirm("Ha you wish you could disappear. You're fated to be stuck on this trail MWAHAHAHA");
+
+}
+confirm("Louise-Lets follow Steve and see what he'll bring us to.");
+confirm("Steve leads you guys through a series of tunnels and caves until finally he brings you guys to a giant door that is covered in chains but only had one tiny lock. Like the ones you put on a diary from the bookfair in elementary. Steve reaches under his belly and pulls out a key and quickly inserts into the keyhole. As soon as the key turned all the chains fell apart and the door opened with slow creak. ");
+
+confirm("When the door opens all the way you see a pile of gold and jems. As well as a limited time edition WWE action figure of the Undertaker. And in the middle of all the treasure is a coffin. You all walk in amazed by all the glory surrounding you guys. You unconciously walk towards the coffin. And you don't realize it till you've reached the coffin and your hands are at the locks opening then. Then all of a sudden the ground begins to rise and a wrestling ring begins to form. Distracted by the sudden cmmotion you don't see the Underrtaker awake from his coffin. And BAM the Undertakers has pulled an old school on you. The Undertaker then grabs you and brings you down in a chokeslam, and then takes ahold of you again and finishes you off with a Tombstone Piledriver. ");
+confirm("Louise- Wow Tuna you really didn't stand a chance agianst him. I thought maybe you'd surpise us with some skill but clearly not.");
+confirm("Tina- Oh Tuna I think I'm in love with that fine specimen who just destroyed you. I'm sorry but I need a beast like him to take care of me and not a little wimp like you. ");
+confirm("Gene-Aww Tuna it's okay if you just got beat six feet into the ground by a monster. Here want half my sandwich? Haahaha you wish loser.");
+confirm("You then knock out into a coma, and is forever forgotten in the coffin of the Undertaker. Meanwhile the Undertaker and the three siblings ride off into the sunset on a majestic unicorn. ");
